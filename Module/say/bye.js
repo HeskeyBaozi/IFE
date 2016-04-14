66308652d1ef2fc4ee5211b9d3f24b9cfd69b183
HeskeyBaozi <895361528@qq.com>
2016-04-14 16:49:55 +0800

    add task8

--- Module/say/bye.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by HeskeyBaozi on 2016/4/3 0003.
- */
-module.exports = function () {
-    console.log('bye!');
-};
--- Module/say/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by HeskeyBaozi on 2016/4/3 0003.
- */
-exports.hello = require('./bye');
-exports.bye = require('./bye');
